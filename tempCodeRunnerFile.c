b33f1628c0f320567327f9383419217d4dff368a
bluberrywaffl <127812144+bluberrywaffl@users.noreply.github.com>
2023-03-28 11:58:00 +0900

    change1

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    int a, b;
+    printf("Input1: ");
+    a = getchar();
+    putchar(a);
+    putchar('\n');
+
+    printf("Input2: ");
+    b = getchar();
+    putchar(b);
+
+
+
+
+
+    return 0;
+}
